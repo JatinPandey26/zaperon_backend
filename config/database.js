b4622d53d09572807f1b279a39c6525933a2f938
Jatin pandey <jatin26pandey@gmail.com>
2023-02-21 00:58:38 +0530

    schema edit

--- config/database.js
+++ config/database.js
@@ -2,6 +2,5 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
-
   console.log(`MongoDB connected with ${connection.host}`);
 };
