502ffa0e347d4a2668dc3ecbff9d740df0c04212
Jatin pandey <jatin26pandey@gmail.com>
2023-02-20 23:12:22 +0530

    json fix

--- server.js
+++ server.js
@@ -19,32 +19,21 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
-
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  next();
-});
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 connect();
 
 app.use("/api/v1/", router);
 
 app.use("/", (req, res) => {
-  res.status(200).json({message : 'Hello'})
+  res.status(200).json({ message: "Hello" });
 });
 app.listen(5000, () => {
   console.log("Example app listening on port 5000");
