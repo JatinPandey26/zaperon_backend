c8acb5414e324062e87dbca04c962c221071ca40
Jatin pandey <jatin26pandey@gmail.com>
2023-02-21 02:09:58 +0530

    schema edit

--- Controller/authController.js
+++ Controller/authController.js
@@ -65,6 +65,6 @@ export const getMeController = async (req, res, next) => {
   return res.status(200).json({
     message: "logged in",
     isAuthenticated: true,
-    user: user.email.split("@")[1],
+    user: user.email.split("@")[0],
   });
 }; 
